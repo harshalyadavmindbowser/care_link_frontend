81434513bf4de31ad033ce52e9a18a5bad8379a5
Jidnya Ganesh Mahajan <jidnya.mahajan@mindbowser.com>
2025-07-28 18:05:06 +0530

    feature: navbar links with avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-
-
-// import './App.css'
-
-function App() {
-
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Appointment from './pages/appointment/appointment';
+import Login from './pages/login/login';
+import Dashboard from './pages/dashboard/patientDashboard';
+import Footer from './components/Footer';
+const Profile = () => <div className="p-4">Profile Page</div>;
+const AppWrapper: React.FC = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
   return (
     <>
-     
-        <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/appointments" element={<Appointment />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      {!isLoginPage && <Footer />}
     </>
-  )
-}
-
-export default App
+  );
+};
+const App: React.FC = () => {
+  return (
+    <Router>
+      <AppWrapper />
+    </Router>
+  );
+};
+export default App;
