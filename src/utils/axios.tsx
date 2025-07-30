6d2f2961d6ea01573b25960d1c3c8f2b8e0408cb
harshal yadav <harshal.yadav@mindbowser.com>
2025-07-30 16:27:24 +0530

    feat: axios interceptor is added

---
+++ src/utils/axios.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+
+const axiosInstance= axios.create({baseURL:'http://localhost:8080/hospitals'});
+
+
+axiosInstance.interceptors.response.use(
+    (response)=>response,
+    (err)=>Promise.reject((err.response && err.response.data || `Something went wrong`))
+); 
+
+export default axiosInstance;
